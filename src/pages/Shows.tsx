ecb7833b9f740c32694b6e18be3fb15c1cd0cbfc
paul <paul.briere94@gmail.com>
2023-01-19 11:54:22 -0500

    Add songkick widget

--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -1,19 +1,15 @@
-import { Box, Container, IconButton, Typography } from "@mui/material";
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { Card, Container } from "@mui/material";
+import { useExternalScripts } from "../hooks/useExternalScript";
 
 const contactEmail = 'info@pside.ca'
 
 export function Shows() {
+  useExternalScripts("//widget.songkick.com/10196177/widget.js")
   return (
-    <Container sx={{my: 4, display: 'flex', flexDirection: 'column', gap: 7, alignItems: 'center'}}>
-      <Box display='flex' flexDirection='column' alignItems='center'>
-        <IconButton href={"mailto:" + contactEmail}>
-          <MailOutlineIcon fontSize="large" />
-        </IconButton>
-        <Typography gutterBottom variant="h5" component="div">
-          {contactEmail}
-        </Typography>
-      </Box>
+    <Container sx={{ my: 4, display: 'flex', flexDirection: 'column', gap: 7, alignItems: 'center' }}>
+      <Card sx={{ borderRadius: 5, width: "95%", minHeight: 300 }}>
+        <a href="https://www.songkick.com/artists/10196177" className="songkick-widget" data-theme="dark" data-font-color="#ffffff" data-background-color="transparent" data-locale="fr" />
+      </Card>
     </Container>
   )
 }
