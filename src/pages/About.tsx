a65bf367ca7043de1b09085e1497f137143435c9
paul <paul.briere94@gmail.com>
2023-01-19 16:14:33 -0500

    updated about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,17 +13,20 @@ function AboutCard() {
       <CardMedia
         sx={{ height: 350 }}
         image="/images/la bande.jpg"
-        title="Cute P-Side"
+        title="La bande des besogneux"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Who dat boy?
+          Miewl Miewl?
         </Typography>
         <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
-          Ever since <b>P-Side</b> dropped his first drum loop in GarageBand on his friend’s iMac in 2009, he was instantly hooked. He immediately proceeded to beg his mom for an acoustic guitar that christmas. Slowly, his janky beats and clumsy strumming fledged themselves out. His musical horizons grew with his desire to create and perform.
+          Il n’est jamais évident de parvenir à créer une œuvre complexe et sensée en ne s’appuyant que sur des observations anecdotiques et profondément absurdes.
+        </Typography>
+        <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
+          Et quand bien même une personne y arriverait, encore faut-il qu'elle parvienne à se faire remarquer. Un jeu d’autant plus compliqué lorsque l’on patauge au milieu d’une vieille métropole saturée de potentiels «*the next big thing*» et gagnants.es de La Voix.
         </Typography>
         <Typography variant="body1" color="text.secondary">
-          Now that his instrumental and production skills are cohesive, he crafts colorful multifaceted music from his constantly expanding inspirations. All this from the comfort of his tiny Montreal apartment.
+          Il est toutefois de ces artistes qui nous désarçonnent de par leur ancrage si particulier à ce monde. Là où la plupart d’entre-nous galopent chaque jour avec des œillères, <b>Muel Muel</b> observe, et constate.
         </Typography>
       </CardContent>
     </Card>
@@ -43,9 +46,9 @@ const ContourBox: React.FC<ContourProps> = ({
 export function About() {
   return (
     <Container sx={{ height: "", my: 4, display: 'flex', flexDirection: 'column', gap: 7, alignItems: 'center' }}>
-      <ContourBox color='rgba(255, 255, 255, 0.1)' padding={0.5}>
-        <ContourBox color='rgba(255, 255, 255, 0.3)' padding={1}>
-          <ContourBox color='#FFF8E7' padding={2.5}>
+      <ContourBox color='rgba(211, 122, 158, 0.1)' padding={0.5}>
+        <ContourBox color='rgba(211, 122, 158, 0.3)' padding={1}>
+          <ContourBox color='rgba(211, 122, 158, 0.8)' padding={2.5}>
             <AboutCard />
           </ContourBox>
         </ContourBox>
